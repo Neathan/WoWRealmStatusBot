842647ef8c2584fcef1a58eeaff2e64e1c7c410f
Neathan <jonathanohrlingg@gmail.com>
2024-01-25 01:04:25 +0100

    feat: Add command support

--- bot.py
+++ bot.py
@@ -6,9 +6,10 @@ from realm_status import RealmStatus
 TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True
+# intents.message_content = True
 
 client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
 
 realmStatus = RealmStatus()
 
@@ -16,25 +17,29 @@ realmStatus = RealmStatus()
 @client.event
 async def on_ready():
     print(f"Logged in as: {client.user}")
+    print(f"Syncing command tree")
+    await tree.sync()
+
+    print(f"Starting webdriver")
     realmStatus.start()
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@tree.command(
+    name="wowstatus",
+    description="Retrieve the current status of all EU realms"
+)
+async def on_wow_status(interaction):
+    # Create/Edit version
+    # loading_embed_msg = discord.Embed(title="Season of Discovery Realm Status", description="Refreshing realm status <a:Loading:1199860788240859227>")
+    # status_msg = await interaction.response.send_message(embed=loading_embed_msg)
+
+    embed_msg = discord.Embed(title="Season of Discovery Realm Status", description="The current status of all EU realms", timestamp=datetime.now())
+    realms = realmStatus.retrieve_realms()
     
-    if message.content.startswith("$status"):
-        loading_embed_msg = discord.Embed(title="Season of Discovery Realm Status", description="Refreshing realm status <a:Loading:1199860788240859227>")
-        status_msg = await message.channel.send(embed=loading_embed_msg)
+    for name, status in realms.items():
+        embed_msg.add_field(name=name, value=RealmStatus.format_status(status))
 
-        embed_msg = discord.Embed(title="Season of Discovery Realm Status", description="The current status of all EU realms", timestamp=datetime.now())
-        realms = realmStatus.retrieve_realms()
-        
-        for name, status in realms.items():
-            embed_msg.add_field(name=name, value=RealmStatus.format_status(status))
+    await interaction.response.send_message(embed=embed_msg)
 
-        await status_msg.delete()
-        await message.channel.send(embed=embed_msg)
 
 client.run(TOKEN)
